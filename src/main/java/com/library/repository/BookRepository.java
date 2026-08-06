1f1af6d60c7406f768e9aacd2c5f70d44908c8f4
aylinnn27 <aylinguliyeva17224@gmail.com>
2026-08-06 21:18:19 +0400

    c5

--- src/main/java/com/library/repository/BookRepository.java
+++ src/main/java/com/library/repository/BookRepository.java
@@ -3,6 +3,7 @@ package com.library.repository;
 import com.library.entity.Book;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -19,4 +20,8 @@ public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificat
     List<Book> findByCategoryName(@Param("categoryName") String categoryName);
 
     List<Book> findByTitleContainingIgnoreCase(String title);
+
+    @Override
+    @EntityGraph(attributePaths = {"author", "categories"})
+    List<Book> findAll();
 }
