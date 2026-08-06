ca1d732686d7fe788e8a701c9d065f83e6604ecf
aylinnn27 <aylinguliyeva17224@gmail.com>
2026-08-06 20:43:25 +0400

    c2

--- src/main/java/com/library/repository/BookRepository.java
+++ src/main/java/com/library/repository/BookRepository.java
@@ -4,10 +4,17 @@ import com.library.entity.Book;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
-
     Page<Book> findByAuthorId(Long authorId, Pageable pageable);
+
+    @Query("SELECT b FROM Book b JOIN b.categories c WHERE LOWER(c.name) = LOWER(:categoryName)")
+    List<Book> findByCategoryName(@Param("categoryName") String categoryName);
+
+    List<Book> findByTitleContainingIgnoreCase(String title);
 }
